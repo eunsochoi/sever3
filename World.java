de16a6fd56ba9e89a5d6604d9cfac99d050f5b43
choieunso <h0119k@naver.com>
2025-05-21 09:40:39 +0900

    five

--- World.java
+++ World.java
@@ -3,5 +3,8 @@ public class World {
     public static void run() {
       System.out.println("202113034");
     }
+    public static void run2() {
+      System.out.println("hello world");
+    }  
   }
   
